fe85dc2f7537e7a1ed37017dedba5a018b092bc1
Goba <cvykarsenij1@gmail.com>
2026-04-18 09:05:23 +0700

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,41 +1,22 @@
-lines_to_write = [
-    "Это первая строка. Тут писать",
+import requests
+from requests.exceptions import RequestException
 
+urls = [
+    "https://github.com/",
+    "https://www.binance.com/en",
+    "https://tomtit.tomsk.ru/",
+    "https://jsonplaceholder.typicode.com/",
+    "https://moodle.tomtit-tomsk.ru/"
 ]
 
-with open("text.txt", "w", encoding="utf-8") as f:
-    for line in lines_to_write:
-        f.write(line + "\n")
+def get_status_text(code: int) -> str:
+    if code == 200:
+        return "доступен"
+    elif code == 403:
+        return "вход запрещен"
+    elif code == 404:
+        return "не найден"
+    else:
+        return "не доступен"
 
-with open("text.txt", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-lines = [line.rstrip("\n") for line in lines]
-
-num_lines = len(lines)
-print(f"Количество строк в файле: {num_lines}")
-
-total_words = 0
-for line in lines:
-    words = line.split()
-    total_words += len(words)
-print(f"Количество слов в файле: {total_words}")
-
-longest_line = max(lines, key=len, default="")
-print(f"Самая длинная строка: {longest_line}")
-
-vowels = set("аеёиоуыэюя")
-consonants = set("бвгджзйклмнпрстфхцчшщ")
-
-total_vowels = 0
-total_consonants = 0
-
-for line in lines:
-    for ch in line.lower():
-        if ch in vowels:
-            total_vowels += 1
-        elif ch in consonants:
-            total_consonants += 1
-
-print(f"Общее количество гласных букв: {total_vowels}")
-print(f"Общее количество согласных букв: {total_consonants}")
+def check_url(url: str, timeout: int = 10) -> tuple:
